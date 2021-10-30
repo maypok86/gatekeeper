328ae4f055a9cdd7ef6638bc55fdb578bff43496
maypok86 <alex.mayshev.86@gmail.com>
2021-10-30 23:26:18 +0300

    add root command

---
+++ cmd/root.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gk",
+	Short: "Gatekeeper service and cli",
+	Long: `Gatekeeper is rate limiting service and cli tool`,
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
