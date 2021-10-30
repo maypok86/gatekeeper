ed2667e9e95ede2439f9a67560e0249776124c7f
maypok86 <alex.mayshev.86@gmail.com>
2021-10-30 23:32:49 +0300

    add version command

---
+++ cmd/version.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version   string
+	buildDate string
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version gk",
+	Long:  `Show version Gatekeeper cli`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print(Format(version, buildDate))
+	},
+}
+
+func Format(version, buildDate string) string {
+	version = strings.TrimPrefix(version, "v")
+
+	var dateStr string
+	if buildDate != "" {
+		dateStr = fmt.Sprintf(" (%s)", buildDate)
+	}
+	if version == "" {
+		version = "develop"
+	}
+
+	return fmt.Sprintf("gk version %s%s\n%s\n", version, dateStr, changelogURL(version))
+}
+
+func changelogURL(version string) string {
+	path := "https://github.com/maypok86/gatekeeper"
+	r := regexp.MustCompile(`^v?\d+\.\d+\.\d+(-[\w.]+)?$`)
+	if !r.MatchString(version) {
+		return fmt.Sprintf("%s/releases/latest", path)
+	}
+
+	url := fmt.Sprintf("%s/releases/tag/v%s", path, strings.TrimPrefix(version, "v"))
+	return url
+}
